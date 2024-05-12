80f7e52d450d1104e8777aefa1f6f936a304e407
hanuteam00 <hanuteam00@gmail.com>
2024-05-12 08:01:07 +0700

    May 11

---
+++ README.md
@@ -0,0 +1,30 @@
+# cypress_cucumber_js_myer
+
+## Part 1: Setup
+
+1. Install npm, cypress and expected browser (eg: Chrome) on your site
+
+2. install dependencies packages
+   ```
+   npm install
+   ```
+
+## Part 2: Run the test
+
+1. Run using Cypress Dashboard
+
+- Click on "create_account.feature" to run the test
+
+2. Run using CLI
+
+- Type to run the test on Chrome browser: npm run cy:chrome
+
+## Part 3: Check report after test
+
+1. Check report by going to local directory
+
+- Click on "create_account.feature" to run the test
+
+2. Open report using CLI
+
+- Type to open report: open ./cypress/reports/html/index.html
---
+++ cypress.config.js
@@ -0,0 +1,81 @@
+/// <reference types="cypress" />
+const { defineConfig } = require("cypress");
+const cypressOnFix = require("cypress-on-fix");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  createEsbuildPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+// Function to set up node events
+async function setupNodeEvents(on, config) {
+  // Fixing issue with hook conflicts
+  // "cypress-on-fix" is required because "cypress-mochawesome-reporter" and "cypress-cucumber-preprocessor" use the same hooks
+  on = cypressOnFix(on);
+
+  // Use the mochawesome reporter plugin
+  require("cypress-mochawesome-reporter/plugin")(on);
+
+  // Add cucumber preprocessor plugin
+  await addCucumberPreprocessorPlugin(on, config);
+
+  // Preprocess files using esbuild
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  // Return the modified config object
+  return config;
+}
+
+// Export Cypress configuration
+module.exports = defineConfig({
+  // Cypress configuration options
+  e2e: {
+    // Environment variables
+    env: {
+      commandDelay: 500, // Delay between commands
+    },
+    // Base URL for tests
+    baseUrl: "https://www.myer.com.au/",
+    // Pattern to search for spec files
+    specPattern: "**/*.feature",
+    // Event setup function
+    setupNodeEvents,
+    // Experimental Studio
+    experimentalStudio: true,
+    // Disable Chrome Web Security
+    chromeWebSecurity: false,
+    // Enable video recording
+    video: true,
+    // Enable screenshots on test failures
+    screenshotOnRunFailure: true,
+    // Trash assets before runs
+    trashAssetsBeforeRuns: true,
+    // Set viewport dimensions
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    // Default command timeout
+    defaultCommandTimeout: 120000, // 30 seconds
+    // Page load timeout
+    pageLoadTimeout: 240000, // 1 minute
+  },
+  // Configuration for Cypress mochawesome reporter
+  reporter: "cypress-mochawesome-reporter",
+  // Reporter options
+  reporterOptions: {
+    charts: true, // Generate charts in HTML report
+    reportPageTitle: "Cypress Inline Mochawesome Reporter",
+    embeddedScreenshots: true, // Embed screenshots within the report
+    inlineAssets: true, // No separate assets folder will be created
+    saveAllAttempts: false, // Save all attempts
+  },
+  
+  // Project ID
+  projectId: "42jpi6",
+});
